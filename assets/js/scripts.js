3ed841c2169d6aae5d614be0b51ced1dd1f0beab
Michael Kolber <michaelkolber@users.noreply.github.com>
2018-07-16 23:33:09 -0400

    Add contact form to site (unfinished)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    window.onscroll = function() {checkScroll();};
+$(function() {
 
-    var header = document.getElementById("nav");
-    var offset = header.offsetTop;
+// Sticky Menu
+window.onscroll = function() {checkScroll();};
+var header = document.getElementById('nav');
+var offset = header.offsetTop;
 
-    function checkScroll() {
-        if (window.pageYOffset > offset) {
-            header.classList.add("nav--sticky");
-        } else {
-            header.classList.remove("nav--sticky");
-        }
+function checkScroll() {
+    if (window.pageYOffset > offset) {
+        header.classList.add('nav--sticky');
+    } else {
+        header.classList.remove('nav--sticky');
     }
+}
+
+
+// Abort redirect on form submit
+$('#contact-form').on('submit', function(e) {
+   e.preventDefault();
+   $.ajax({
+       url: $(this).attr('action'),
+       type: 'POST',
+       data: $(this).serialize(),
+       beforeSend: function() {
+           $('#contact-form__submit').val('Sending...');
+       },
+       success: function(data) {
+           // $('.contact-form__overlay').addClass('contact-form__overlay--visible');
+           $('#contact-form__submit').val('Sent!');
+           $('#contact-form').each(function(){
+                this.reset();
+            });
+       }
+   });
+});
+
 });
